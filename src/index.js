0ecf23552346f2b9f0c70d3ae211f0fab5b36be1
robertowusu <robertowusu4199@gmail.com>
2021-07-08 19:27:27 -0800

    adding firebase

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
 
-
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
